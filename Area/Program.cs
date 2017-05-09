70c977aaec2bd913ce59003d245dcd40f4fc8e71
James Crow <jamescrowvideo@gmail.com>
2017-05-08 19:49:13 -0500

    Finished Studio

---
+++ Area/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad;
+            String rad_str;
+           
+            
+            do
+            {
+                Console.Write("Enter a radius: ");
+                rad_str = Console.ReadLine();
+
+                bool test = double.TryParse(rad_str, out rad);
+
+                if (test)
+                {
+                    rad = double.Parse(rad_str);
+                    double area = (3.14 * rad * rad);
+
+                    Console.WriteLine();
+                    Console.WriteLine("The radius of a circle of radius {0} is: {1}", rad, area);
+                    Console.ReadLine();
+                };
+             } while (rad_str != "exit");
+                
+        }   
+    }
+}
---
+++ LargestInt/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number_array = new int[5];
+
+            for(int i = 0; i < number_array.Length; i++)
+            {
+                Console.WriteLine("Number {0}: ", i + 1);
+                number_array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxVal = maxValue(number_array);
+
+            Console.WriteLine("The largest number is: " + maxVal);
+            Console.ReadLine();
+        }
+
+        public static int maxValue(int[] array)
+        {
+
+            int maxVal = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxVal)
+                { 
+                    maxVal = array[i];
+                }
+            }
+            return maxVal;
+        }
+    
+    }
+
+}
